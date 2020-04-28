305fc991f03ce2039c42c68ccc4c32287bf61087
Victor <victorleon77@gmail.com>
2020-04-28 21:22:52 +0200

    Primer Commit

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from './login.component';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
